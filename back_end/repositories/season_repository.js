b5af4c8603a0e4d100009ed3f979a49b66cd3e93
minh360 <minhtrai23112001@gmail.com>
2022-08-07 22:24:00 +0700

    Update

---
+++ back_end/repositories/season_repository.js
@@ -0,0 +1,31 @@
+const Season = require('../models/season')
+const {STATE_SEASON} = require("../state_season");
+
+class SeasonRepository {
+    constructor(model) {
+        this.model = model;
+    }
+    create(object){
+        console.log(this.countDocument())
+        const newSeason = {
+            season: this.countDocument() + 1,
+            timeBegin: object.time,
+            state: STATE_SEASON.PLAYING,
+            coinWin: object.coin,
+            nameWin: object.name
+        };
+        const season = new this.model(newSeason);
+
+        return season.save();
+    }
+    countDocument(){
+        return this.model.countDocuments()
+    }
+    findBySeason(season){
+        return this.model.findOne({season: season})
+    }
+    updateBySeason(object){
+        return this.model.findOneAndUpdate({season: object.season}, { $set: {coinWin: object.coin, nameWin: object.name}})
+    }
+}
+module.exports = new SeasonRepository(Season)
